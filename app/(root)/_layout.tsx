554eb468510179598f766e422ee2f6b7f7fcad01
MaksLevin <maksim.levin@hiqo-solutions.com>
2024-12-20 16:06:16 +0300

    Added authentication

--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -1,7 +1,13 @@
 import { Stack } from 'expo-router';
 
+import '../../global.css';
+
 const Layout = () => {
-  return <Stack></Stack>;
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
+  );
 };
 
 export default Layout;
